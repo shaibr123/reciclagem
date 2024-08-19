33a7d5ffae8252d8133383edc99bee868503a834
Shaiane Rodrigues <130679811+shaibr123@users.noreply.github.com>
2024-08-19 09:29:13 -0300

    or

--- script.js
+++ script.js
@@ -1,55 +1,88 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-
 const caixaResultado = document.querySelector(".caixa-resultado");
-
-const caixaResultado = document.querySelector(".texto-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-     enunciado: "1.Quais são os principais materiais que podem ser reciclados e como devem ser preparados antes da coleta?",
-      alternativas: [
-           "A reciclagem, possibilita desempenhar um papel fundamental na detecção e prevenção de ameaças à saude.",
-           "Também pode ser uma ferramenta para a reduçao de novos transmissores de doenças."
-       ]
-   },
-    
-    {
-     enunciado: "Quais são os desafios mais comuns enfrentados pelos sistemas de reciclagem em áreas urbanas e rurais?",
-      alternativas: [
-           "falta de saneamento basico.",
-            "problema na coleta do lixo sendo ele reciclado ou nao."
-       ]
+        enunciado: "A reciclagem, possibilita desempenhar um papel fundamental na detecção e prevenção de ameaças à saude.",
+        alternativas: [
+            {
+                texto: "Também pode ser uma ferramenta para a reduçao de novos transmissores de doenças.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Quais são os desafios mais comuns enfrentados pelos sistemas de reciclagem em áreas urbanas e rurais?",
+                afirmacao: "falta de saneamento basico.", "problema na coleta do lixo sendo ele reciclado ou nao."
+               }
+          ]
     },
     {
         enunciado: "Como a reciclagem contribui para a preservação ambiental e a redução da pegada de carbono?",
-      alternativas: [
-               "diminuindo a puluicao, e reduzindo o gás carbonico na atmosfera.",
-               "poluindo menos e causando menos erozao."
-       ]
+        alternativas: [
+            {
+                texto: "diminuindo a puluicao, e reduzindo o gás carbonico na atmosfera.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "trabalhando a influência das tradições e culturas em nosso cotidiano",
+                afirmacao: "diminuindo a puluicao, e reduzindo o gás carbonico na atmosfera.",
+            }
+        ]
+    },
+    {
+        enunciado: "como os cidadoes podem se envover mais ativamente em progrmas de reciclagem",
+        alternativas: [
+            {
+                texto: "Buscando informação na legislação da saúde",
+                afirmacao: "reclicando corretamente os lixos"
+            },
+            {
+                texto: "de que forma a reciclagem pode reduzir o impacto do lixo nos oceanos",
+                afirmacao: "diminuindo o numero de plasticos jogados nas praias"
+            }
+        ]
     }
-     ];
+   
+];
 
-     let atual = 0;
-     let perguntaAtual;
 
-function mostraPergunta(){
-     perguntaAtual = perguntas[atual];
-     caixaPerguntas.textContent = perguntaAtual.enunciado;
-     caixaAlternativas.textContent= "";
-     mostraAlternativas();
-    
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
     }
-function mostraAlternativas(){
-   for(const alternativa of perguntaAtual.alternativas){
-    const botaoAlternativas= document.creatElement("button");
-    botaoAlternativas.textContent = alternativa;
-    caixaAlternativas.appendChild(botaoAlternativas);
-
-   }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-     mostraPergunta();
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacao;
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
