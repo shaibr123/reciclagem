6dfa1afd06061d989cd95f3ac04bd327c6e76ba1
Shaiane Rodrigues <130679811+shaibr123@users.noreply.github.com>
2024-09-02 09:26:06 -0300

    or

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Em 2024...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
